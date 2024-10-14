781a41db3740ea5465b0a3777479e84fff23adfa
Jam <jam29n@gmail.com>
2024-10-14 16:12:16 +0200

    1st

---
+++ src/main/java/org/openlab/cover/CoverApplication.java
@@ -0,0 +1,13 @@
+package org.openlab.cover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/openlab/cover/CoverApplicationTests.java
@@ -0,0 +1,13 @@
+package org.openlab.cover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
